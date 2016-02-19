bda1b9bbf4eb6207d1f3e29829176db4d8f71e00
Olaf Veerman <olaf.veerman@gmail.com>
2016-02-19 11:33:30 -0500

    Add port

--- app/config/production.js
+++ app/config/production.js
@@ -2,6 +2,9 @@
  * App config for production.
  */
 module.exports = {
+  connection: {
+    port: 3000
+  },
   environment: 'production',
   ghUser: 'open-contracting-partnership',
   ghRepo: 'ocp-data'
